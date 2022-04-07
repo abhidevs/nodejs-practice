ae2712a1590e253650590e40db78d85fcf8dc52d
Abhik Das <abhikdas98765@gmail.com>
2022-04-07 22:04:07 +0530

    added code for parsing message and writing to a file

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  //   console.log(req.url, req.method, req.headers);
+  const { url, method } = req;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+      console.log(chunk);
+    });
+
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
 
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>Hello from NodeJs</title></head>");
-
-  if (req.url === "/home") 
-    res.write("<body><h1>Welcome home</h1></body>");
-  else if (req.url === "/about")
-    res.write("<body><h1>Welcome to About Us page</h1></body>");
-  else if (req.url === "/node")
-    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
-  else 
-    res.write("<body><h1>Welcome</h1></body>");
-
+  res.write("<body><h1>Welcome to my Node Js server</h1></body>");
   res.write("</html>");
   res.end();
 });

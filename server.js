3eb92187635854d5c853edf20a32dcf90bf23b98
Abhik Das <abhikdas98765@gmail.com>
2022-04-07 16:51:40 +0530

    added code for sending response from server

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("Abhik Das");
+  //   console.log(req.url, req.method, req.headers);
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Hello from NodeJs</title></head>");
+
+  if (req.url === "/home") 
+    res.write("<body><h1>Welcome home</h1></body>");
+  else if (req.url === "/about")
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+  else if (req.url === "/node")
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+  else 
+    res.write("<body><h1>Welcome</h1></body>");
+
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(4000, console.log('Server running on port 4000'));
+server.listen(4000, console.log("Server running on port 4000"));

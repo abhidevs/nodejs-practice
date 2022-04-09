cf548633b12929504b9e5b36ac4572ea8d51c796
Abhik Das <abhikdas98765@gmail.com>
2022-04-09 09:24:57 +0530

    added code for express

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Nodejs Practice
 I'm using this repo for revising nodeJs
+
+Code examples for ExpressJS
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require("http");
-const routes = require("./routes");
-// const { handler, someText } = require("./routes");
 
-// console.log(someText);
+const express = require("express");
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(4000, console.log("Server running on port 4000"));
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from Express</h1>");
+});
+
+app.listen(4000, console.log("Server running on port 4000"));
